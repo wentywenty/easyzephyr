0652fc8f2444091369e3742d36ee74d04c4b560b
wentywenty <2321901849@qq.com>
2025-01-28 21:36:32 +0800

    准备模仿

--- install.ps1
+++ install.ps1
@@ -1,36 +1,22 @@
-安装scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop install git
 
+scoop install ninja wget uv
+git clone https://github.com/wentywenty/zephyr.git
 
-安装依赖项
-scoop install git 
-scoop install ninja wget uv gh
-
-克隆存储库
-gh auth login
-gh repo clone wentywenty/zephyr
-
-安装依赖包
-cd zephyr
 scoop install ./dtc.json 
 scoop install ./gperf.json 
 scoop install ./nrfutil.json
 
-开启虚拟环境
 uv python install 3.11
-
 uv venv
-.venv\Scripts\activate       
+# .venv\Scripts\activate       
 
-安装sdk
-uv pip install west
+# uv pip install west
 
-west init -m https://github.com/nrfconnect/sdk-nrf
+# west init -m https://github.com/nrfconnect/sdk-nrf
 
-west update
+# uv pip install -r zephyr/scripts/requirements.txt -r nrf/scripts/requirements.txt -r bootloader/mcuboot/scripts/requirements.txt
 
-west zephyr-export
-
-uv pip install -r zephyr/scripts/requirements.txt -r nrf/scripts/requirements.txt -r bootloader/mcuboot/scripts/requirements.txt
-
-zephyr\zephyr-env.cmd
-
+# zephyr\zephyr-env.cmd
