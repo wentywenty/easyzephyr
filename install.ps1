860b3eec7ff19ff02b6df9abe371d922df41554a
wentywenty <2321901849@qq.com>
2025-01-28 21:16:57 +0800

    准备开始交互

--- install.ps1
+++ install.ps1
@@ -1,24 +1,36 @@
+安装scoop
+
+
+安装依赖项
 scoop install git 
 scoop install ninja wget uv gh
 
+克隆存储库
 gh auth login
 gh repo clone wentywenty/zephyr
-gh repo clone wentywenty/nrfutil
 
+安装依赖包
 cd zephyr
 scoop install ./dtc.json 
 scoop install ./gperf.json 
-scoop install ../nrfutil/nrfutil.json
+scoop install ./nrfutil.json
 
+开启虚拟环境
 uv python install 3.11
-uv venv
 
+uv venv
+.venv\Scripts\activate       
+
+安装sdk
 uv pip install west
 
 west init -m https://github.com/nrfconnect/sdk-nrf
 
 west update
 
+west zephyr-export
+
 uv pip install -r zephyr/scripts/requirements.txt -r nrf/scripts/requirements.txt -r bootloader/mcuboot/scripts/requirements.txt
 
+zephyr\zephyr-env.cmd
 
